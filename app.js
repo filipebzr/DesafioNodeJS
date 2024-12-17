cf00519d97682a3351c7978ad82549df2db7abab
FilipeBzr <fil@pe.com>
2024-12-17 15:21:11 -0300

    Adiciona .gitignore para ignorar node_modules

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const db = require('./db/connection').default;
+const bodyParser = require('body-parser');
+
+const PORT = 3000;
+
+app.listen(PORT, function () {
+    console.log(`O express está rodando na porta ${PORT}`);
+});
+
+//body parser
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// db connection 
+db.authenticate()
+    .then(() => {
+        console.log('conectou com o banco');
+    })
+    .catch(err => {
+        console.log("ocorreu um erro ao conectar", err)
+    });
+
+
+// routes
+app.get('/', (req, res) => {
+    res.send('Está funcionando e agora vai');
+});
+
+//jobs routes
+app.use('/jobs', require('./routes/jobs'));
